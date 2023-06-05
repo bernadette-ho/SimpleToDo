4de12e036adf37d0df12664cb67ca8d80ffe6b62
22023330 <22023330@myrp.edu.sg>
2023-06-05 22:14:31 +0800

    ver2

--- app/src/main/java/sg/edu/rp/c346/id22023330/simpletodo/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22023330/simpletodo/MainActivity.java
@@ -32,26 +32,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        etNewTodo=findViewById(R.id.etToDo);
-        btnAdd=findViewById(R.id.btnAdd);
-        btnDel=findViewById(R.id.btnDel);
-        btnClear=findViewById(R.id.btnClear);
-        lvTodo=findViewById(R.id.lvToDo);
-        spinnerOptions=findViewById(R.id.spinnerOptions);
+        etNewTodo = findViewById(R.id.etToDo);
+        btnAdd = findViewById(R.id.btnAdd);
+        btnDel = findViewById(R.id.btnDel);
+        btnClear = findViewById(R.id.btnClear);
+        lvTodo = findViewById(R.id.lvToDo);
+        spinnerOptions = findViewById(R.id.spinnerOptions);
 
-        alTodo=new ArrayList<>();
-        aaTodo=new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alTodo);
+        alTodo = new ArrayList<>();
+        aaTodo = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alTodo);
         lvTodo.setAdapter(aaTodo);
 
         spinnerOptions.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 String selectedOption=parent.getItemAtPosition(position).toString();
-                if (selectedOption.equals("Add a new task")){
+                if (selectedOption.equals("Add a new task")) {
                     etNewTodo.setHint("Type in new task here");
                     btnDel.setEnabled(false);
                     btnAdd.setEnabled(true);
-                }else if (selectedOption.equals("Remove a task")){
+                }
+                else if (selectedOption.equals("Remove a task")) {
                     etNewTodo.setHint("Type in the index of the task to be removed");
                     btnDel.setEnabled(true);
                     btnAdd.setEnabled(false);
@@ -60,17 +61,17 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-                // do nothing
+
             }
         });
 
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String newTodo=etNewTodo.getText().toString();
+                String newTodo = etNewTodo.getText().toString();
                 alTodo.add(newTodo);
                 aaTodo.notifyDataSetChanged();
-                etNewTodo.setText(null);
+                etNewTodo.setText("");
             }
         });
 
@@ -79,12 +80,13 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String indexStr=etNewTodo.getText().toString();
                 if (!indexStr.isEmpty()){
-                    int index=Integer.parseInt(indexStr);
-                    if (index>=0&&index<alTodo.size()){
+                    int index = Integer.parseInt(indexStr);
+                    if (index >= 0 && index < alTodo.size()) {
                         alTodo.remove(index-1);
                         aaTodo.notifyDataSetChanged();
-                        etNewTodo.setText(null);
-                    }else{
+                        etNewTodo.setText("");
+                    }
+                    else{
                         Toast.makeText(MainActivity.this, "Wrong index number", Toast.LENGTH_SHORT).show();
                         Log.d("MainActivity", "Wrong index number: " + index);
                     }
